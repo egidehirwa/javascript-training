506ae5c4b4afb14413607bd1fb84b061f7d92b68
Egide Hirwa <egidehirwa@Messoins-Laptop.local>
2023-11-02 23:41:59 +0100

    data types, variables

--- scipt.js
+++ scipt.js
@@ -1,2 +1,39 @@
 let js = 'amazing';
-if(js === 'amazing') alert('Hello')
+
+let firstName = 'Bob'
+console.log(firstName)
+
+console.log(js)
+
+let myNextJob = 'Programmer'
+
+console.log(myNextJob)
+
+/* 1st assignment - Values and Variables */
+
+let country = 'Belgium';
+let continent = 'Europe';
+let population = '11 Million';
+
+console.log(`l live in ${country}, located in ${continent} with ${population} inhabitants`);
+
+
+/* Data Types */
+
+let jsIsFun = true;
+console.log(jsIsFun)
+
+console.log(typeof jsIsFun)
+console.log(typeof 23)
+console.log(typeof 'jsIsFun')
+
+const isIsland = false;
+let language
+
+console.log(isIsland);
+console.log(typeof isIsland, typeof population, typeof country, typeof language)
+
+
+/* Declaring variables */
+
+language = 'French';
